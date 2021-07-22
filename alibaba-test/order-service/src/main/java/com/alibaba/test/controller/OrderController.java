94316ab832cc400477330fad8ab61fd6ff30330d
recheard <2394806176@qq.com>
2021-07-22 11:36:53 +0800

    feat:通过负载均衡器远程调用

--- alibaba-test/order-service/src/main/java/com/alibaba/test/controller/OrderController.java
+++ alibaba-test/order-service/src/main/java/com/alibaba/test/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.alibaba.test.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,9 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     int index = 0;
 
     @GetMapping("test")
@@ -38,5 +42,25 @@ public class OrderController {
         String result = restTemplate.getForObject(uri + "/test/test1", String.class);
         return "order service get result for member-service:" + result;
     }
+
+
+    /**
+     * 通过负载均衡器访问其他服务
+     * @return
+     */
+    @GetMapping("test_loadBalancer")
+    public String testLoadBalancer () {
+        //获取指定服务的节点列表
+        List<ServiceInstance> memberServiceList = discoveryClient.getInstances("member-service");
+        //通过负载均衡器获取服务实例, 默认轮询
+        ServiceInstance choose = loadBalancerClient.choose("member-service");
+        URI uri = choose.getUri();
+        //通过restTemplate访问
+        String result = restTemplate.getForObject(uri + "/test/test1", String.class);
+        choose.getUri();
+        return "order service get result for member-service:" + result;
+    }
+
+
 
 }
